ed13f86e35477268e4888ef62e1e478f13567113
Nam Nguyen <ducnam1719@gmail.com>
2025-02-09 15:54:17 +0700

    feat: update sidebar with the workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -10,6 +11,8 @@ export const Sidebar = () => {
       <Link href={PATHS.DASHBOARD} className="flex justify-center">
         <Image src="/logo.svg" alt="Logo" width={56} height={56} />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
